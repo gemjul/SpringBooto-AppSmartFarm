d2b4bc543d4226c1f69fd4557b87c88c972b6506
JULIAN AMADO ROG <JULIAN AMADO ROG@ASUSROG>
2019-11-17 21:38:05 -0500

    Comit incial con eclipse

---
+++ appsmartfarm/src/main/java/com/appsmartfarm/appsmartfarm/AppsmartfarmApplication.java
@@ -0,0 +1,13 @@
+package com.appsmartfarm.appsmartfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsmartfarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsmartfarmApplication.class, args);
+	}
+
+}
---
+++ appsmartfarm/src/test/java/com/appsmartfarm/appsmartfarm/AppsmartfarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsmartfarm.appsmartfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsmartfarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
